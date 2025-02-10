ee08cebe0e755cd3dc35e1db274862efbd7225bb
joaootaviofm <joaootaviofm@gmail.com>
2025-02-10 11:26:58 -0300

    added html and style

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,9 +3,27 @@ import { useState } from 'react'
 
 function ToDoList(){
 
+    const [newTask, setNewTask] = useState("")
+    const [tasks, setNewTasks] = useState(["LAvar coisa", "fazer x","fazer y"])
+
+    function handleInputChange(event){
+        setNewTask(event.target.value)
+    }
+
     return (
-        
-    )
+        <div className="container">
+            <h1>To Do List</h1>
+            <div className='inputs'>
+                <input type="text" placeholder='Enter task...' value={newTask} onChange={handleInputChange}/>
+                <button className='addButton'>+ Add Task</button>
+            </div>
+            <ol>
+                {tasks.map((task, index) => 
+                    <li key={index}>{task}</li>
+                )}
+            </ol>
+        </div>
+    )   
 }
 
 export default ToDoList 
